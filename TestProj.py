0d04bc779129e2cef8506c1e8e43f1d55fe8de7e
Shreenidhi <s.shreenidhi19@gmail.com>
2022-06-30 13:07:22 +0530

    added : Special variables

---
+++ TestProj.py
@@ -0,0 +1,11 @@
+from calculation import *
+def fun1():
+    add()
+    print('one')
+def fun2():
+    print('two')
+
+def main():
+    fun1()
+    fun2()
+main()
---
+++ calculation.py
@@ -0,0 +1,11 @@
+def add():
+    print('addition')
+def sub():
+    print('subtraction')
+
+def main():
+    add()
+    sub()
+
+if __name__=="__main__":
+    main()
